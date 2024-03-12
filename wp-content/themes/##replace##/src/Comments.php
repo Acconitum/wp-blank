5b2cacf07de870fe1d3f993897e3f46eab422b6d
Marc Mentha <marc.mentha@staempfli.com>
2024-03-12 10:16:42 +0100

    [TASK] Add theme support and remove comments

---
+++ wp-content/themes/##replace##/src/Comments.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Staempfli;
+
+class Comments
+{
+
+    /**
+     * Add actions and hooks to WordPress core to remove comments completly
+     * 
+     * @static
+     * @return void
+     */
+    public static function addActions()
+    {
+        add_action('admin_init', __CLASS__ . '::removeComments');
+        add_action('admin_menu', __CLASS__ . '::removeCommentsMenu');
+        add_action('init', __CLASS__ . '::removeCommentsMenuInAdminBar');
+
+        add_filter('comments_array', '__return_empty_array', 10, 2);
+        add_filter('comments_open', '__return_false', 20, 2);
+        add_filter('pings_open', '__return_false', 20, 2);
+    }
+
+    public static function removeComments()
+    {
+        $types = get_post_types();
+        foreach ($types as $type) {
+            if(post_type_supports($type, 'comments')) {
+                remove_post_type_support($type, 'comments');
+                remove_post_type_support($type, 'trackbacks');
+            }
+        }
+        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+        global $pagenow;
+        
+        if ($pagenow === 'edit-comments.php' || $pagenow === 'options-discussion.php') {
+            wp_safe_redirect(admin_url());
+            exit;
+        }
+    }
+
+    public static function removeCommentsMenu()
+    {
+        remove_menu_page('edit-comments.php');
+        remove_submenu_page( 'options-general.php', 'options-discussion.php' );
+    }
+
+    public static function removeCommentsMenuInAdminBar()
+    {
+        if (is_admin_bar_showing()) {
+            remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+        }
+    }
+}
--- wp-content/themes/##replace##/src/Theme.php
+++ wp-content/themes/##replace##/src/Theme.php
@@ -18,6 +18,40 @@ class Theme
     public static function addActions()
     {
         add_action('wp_enqueue_scripts', __CLASS__ . '::addStyleSheet');
+        add_action('after_setup_theme', __CLASS__ . '::addThemeSupport');
+        add_action('widgets_init', __CLASS__ . '::addWidgets');
+        add_action('init', __CLASS__ . '::removeBackendEditor');
+
+
+        Comments::addActions(); // Comment out if you like to enable comments
+    }
+
+    public static function addThemeSupport()
+    {
+	    add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_theme_support('customize-selective-refresh-widgets');
+    }
+
+    public static function addWidgets()
+    {
+        register_sidebar(
+            ['name'             => esc_html__( 'Sidebar', 'test' ),
+                'id'            => 'sidebar-1',
+                'description'   => esc_html__( 'Add widgets here.', 'test' ),
+                'before_widget' => '<section id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</section>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+            ]
+        );
+    }
+
+    public static function removeBackendEditor()
+    {
+        if (!defined('DISALLOW_FILE_EDIT')) {
+            define('DISALLOW_FILE_EDIT', true);
+        }
     }
 
     /**
